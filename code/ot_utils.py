fbb6dae0e71ca36260d527f41949fd35baba0972
Wornbard <jakub.wornbard@gmail.com>
2021-07-13 19:28:36 +0100

    added OT with sinks/sources

--- code/ot_utils.py
+++ code/ot_utils.py
@@ -4,7 +4,9 @@ def mina_u(H,a,epsilon): return -epsilon*np.log( np.sum(a * np.exp(-H/epsilon),0
 def minb_u(H,b,epsilon): return -epsilon*np.log( np.sum(b * np.exp(-H/epsilon),1) )
 
 
-def mina(H,a,epsilon): return mina_u(H-np.min(H,0),a,epsilon) + np.min(H,0)
+def mina(H,a,epsilon):
+    #print(mina_u(H-np.min(H,0),a,epsilon) + np.min(H,0))
+    return mina_u(H-np.min(H,0),a,epsilon) + np.min(H,0)
 def minb(H,b,epsilon): return minb_u(H-np.min(H,1)[:,None],b,epsilon) + np.min(H,1)
 
 def Sinkhorn(C,a,b,epsilon,f,niter = 500):#cost matrix, histograms    
