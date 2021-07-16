2eb865c104c45d58c0468534f3629a8f26a3c4f7
Wornbard <jakub.wornbard@gmail.com>
2021-07-16 15:20:04 +0100

    added comments that explain how to use it

--- code/motion_utils.py
+++ code/motion_utils.py
@@ -2,11 +2,24 @@ import numpy as np
 import pandas as pd
 
 class particle_system:
+    '''
+    Keep's track of the system's size, state and the particels in it
+    Constructor takes a list of length equal to the system's dimension
+    For [d1,d2,...,dk] the domain on which the particle can move is [0,d1]x[0,d2]x ... x[0,dk]
+    '''
     def __init__(self,grid):#grid:integer tuple
         self.grid=grid
         self.particles=[]
         self.dim=len(self.grid)
         
+    '''
+    n : number of particles to insert
+    update_pos : a function which is passed to the constructor of particle, used for evolving it in time (see description of particle class for details)
+    update_intensity : as above
+    start_pos : if None, then generates randomly, otherwise a list of length n containing the initial positions as lists of length self.dim
+    intensity : if None, then set to 1, otherwise a list of length n
+
+    '''
     def insert_particles(self,n,update_pos,update_intensity,start_pos=None,intensity=None):
         if start_pos is None:
             pos_data=[np.random.uniform(low=0, high=self.grid[i], size=n) for i in range(self.dim)]
@@ -15,10 +28,15 @@ class particle_system:
             intensity=[1 for i in range(n)]
             
         for i in range(n):
-            self.particles.append(particle(self.grid,self.dim,start_pos[i],update_pos,intensity[i],update_intensity))
+            self.particles.append(particle(self.grid,start_pos[i],update_pos,intensity[i],update_intensity))
+    
+    '''
+    updates the state of each particle in the system
+    '''
     def evolve(self,dt):
         for p in self.particles:
             p.evolve(dt)
+
     def positions(self):
         return [p.pos for p in self.particles]
     def intensities(self):
@@ -26,9 +44,17 @@ class particle_system:
 
 
 class particle:
-    def __init__(self,grid,dim,start_pos,update_pos,intensity,update_intensity):
+    '''
+    grid : as in particle_system
+    start_pos : initial position in a list
+    update_pos : assigned as a method of the class so the arguments it takes are : instance of class particle, time increment
+    intenisty : initial intensity
+    update_intsnity : as in update_pos. Note that the current method to make a particle vanish is to set its intensity to 0 so this method should handle that
+    '''
+
+    def __init__(self,grid,start_pos,update_pos,intensity,update_intensity):
         self.grid=grid#dimensions of the space it's restricted to
-        self.dim=dim
+        self.dim=len(self.grid)
         
         self.pos=start_pos#current position
         self.__class__.update_pos=update_pos
@@ -40,10 +66,24 @@ class particle:
         self.pos=self.update_pos(dt)
         self.intensity=self.update_intensity(dt)
 
+'''
+checks if a point is contained in a grid
+'''
 def inside_grid(pos,grid):#assume grid has leftmost bottom corner in 0, check if a given point is inisde of it
     inside_dimensions=[pos[i]>=0 and pos[i]<=grid_dim for i,grid_dim in enumerate(grid)]
     return all(inside_dimensions)
 
+
+'''
+brownian motion of a single particle in n dimensions
+part : particle
+dt: time increment
+D : diffusion coefficient
+drift : list of length equal to dimension if we want to have a drift in the motion
+
+Note that when passing this to a constructor we need to specify the D and drift parameter with a lambda i.e.
+pass lambda x,y : brownian_update(x,y,some_D,some_drift)
+'''
 def brownian_update(part,dt,D,drift=None):
     if drift is None:
         drift=[0]*part.dim
@@ -59,6 +99,10 @@ def brownian_update(part,dt,D,drift=None):
     else:#need to figure out where and when it intersects the boundary. For now let's just resample
         return brownian_update(part,dt,D,drift)
 
+
+'''
+Takes an output file of thunderstorm in csv, frame number (starting from 1), and returns two lists of positions and intensities of every observed particles in a given frame
+'''
 def thunderstorm_extract(directory,frame_id):
     df=pd.read_csv(directory)
     frame=df.loc[df['frame'] == frame_id]
